ac49c7a83c23f93539563d549c9d18ef0d807a47
jakobdubeau <jakobdubeau@gmail.com>
2026-04-02 17:45:39 -0400

    write info predicate

--- main.pl
+++ main.pl
@@ -1,1 +1,7 @@
 #initial commit of main
+
+writeMatchInfo(ResidentID,ProgramID):-
+    resident(ResidentID,name(FN,LN),_),
+    program(ProgramID,TT,_,_),write(LN),write(','),
+    write(FN),write(','),write(ResidentID),write(','),
+    write(ProgramID),write(','),writeln(TT).

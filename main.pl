577a8b8c15c309bdd540c01f404c0b759eb92e75
jakobdubeau <jakobdubeau@gmail.com>
2026-04-02 18:06:09 -0400

    core predicates

--- main.pl
+++ main.pl
@@ -1,7 +1,18 @@
-#initial commit of main
+# display solution
+writeMatchInfo(ResidentID, ProgramID) :-
+    resident(ResidentID, name(FN,LN), _),
+    program(ProgramID, TT, _, _), write(LN), write(','),
+    write(FN), write(','), write(ResidentID), write(','),
+    write(ProgramID), write(','), writeln(TT).
 
-writeMatchInfo(ResidentID,ProgramID):-
-    resident(ResidentID,name(FN,LN),_),
-    program(ProgramID,TT,_,_),write(LN),write(','),
-    write(FN),write(','),write(ResidentID),write(','),
-    write(ProgramID),write(','),writeln(TT).
+# computes the rank of a resident in program's rol
+rankInProgram(ResidentID, ProgramID, Rank) :-
+
+# find least preferred resident in program's list
+leastPreferred(ProgramID, ResidentIDsList, LeastPreferredResidentID, RankOfThisResident) :-
+
+# checks if a resident is matched
+matched(ResidentID, ProgramID, Matcheset) :-
+
+# assign a program to the resident
+offer(ResidentID, currentMatchset, newMatchset) :-

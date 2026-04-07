09ccf3b50483ff28583844b0d10cd8e72094b696
jakobdubeau <jakobdubeau@gmail.com>
2026-04-06 20:33:32 -0400

    gale_shapely/0

--- main.pl
+++ main.pl
@@ -1,27 +1,8 @@
-% match set looks like:
-% [
-%   match(nrs, [517,574]),
-%   match(obg, [616])
-% ]
-
-%TEMP FOR TESTING PURPOSES
-%--------------------------------------------------------
-resident(574, name(salvatore,williams), [nrs,hep,mmi]).
-resident(517, name(rosalie,frederick), [nrs,mmi]).
-resident(126, name(indie,medrano), [mmi,nrs]).
-resident(828, name(emma,tremmo), [obg,nrs]).
-resident(403, name(aspyn,olson), [hep,nrs]).
-resident(226, name(zev,jarvis), [mmi,hep,nrs]).
-resident(913, name(camille,paquet), [obg,mmi,hep]).
-resident(773, name(marie,clown), [obg]).
-resident(616, name(laurent,robert), [obg,mmi,hep,nrs]).
-resident(377, name(tom,tan), [mmi,obg]).
-program(nrs, "Neurosurgery",4, [574,517,403,828,226,126]).
-program(obg, "Obstetrics and Gynecology",3, [616,828,773,913]).
-program(mmi, "Microbiology",1, [574,517,226,913,377,126]).
-program(hep, "Hematological Pathology",2, [403,574,913,616,226]).
-%--------------------------------------------------------
-
+% high-level gale_shapely/0
+gale_shapely :-
+    initialMs(InitialMatchSet),
+    stable_loop(InitialMatchSet, FinalMatchSet),
+    write_solution(FinalMatchSet).
 
 % display solution
 writeMatchInfo(ResidentID, ProgramID) :-
@@ -130,5 +111,4 @@ stable_loop(CurrentMatchSet, FinalMatchSet) :-
     ;
         stable_loop(NewMatchSet, FinalMatchSet)
     ).
-
 
